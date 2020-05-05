a209d88e261c792852f778eb6332fc4be6dd21ea
Kilian Kreibich <kiliankreibich63@gmail.com>
2020-05-05 09:59:00 +0200

    Final changes for 1.0 release

--- RPC_Form.Designer.cs
+++ RPC_Form.Designer.cs
@@ -149,7 +149,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(443, 421);
+            this.ClientSize = new System.Drawing.Size(443, 416);
             this.Controls.Add(this.lb_usr);
             this.Controls.Add(this.btn_rem);
             this.Controls.Add(this.pictureBox1);
